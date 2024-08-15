ceda69100405d4c4f86da88259a9471428b64636
Lokinado <hisboak@gmail.com>
2024-08-15 17:48:40 +0200

    Added boilerplate code for CommandParser

---
+++ GameClient/Source/CommandParser.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameClient.Source;
+
+/// <summary>
+/// Provides parser.
+/// </summary>
+internal static class CommandParser
+{
+    /// <summary>
+    /// Parse method that recieves data from event.
+    /// </summary>
+    /// <param name="sender">Event sender.</param>
+    /// <param name="e">Event arguments.</param>
+    public static void Parse(object? sender, EventArgs e)
+    {
+    }
+}
