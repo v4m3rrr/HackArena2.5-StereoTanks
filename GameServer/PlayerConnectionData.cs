b5bc98829faa68dccde08dcd159b2b878f8161df
Wiktor Jaworski <wijaweq@gmail.com>
2024-09-23 09:29:59 +0200

    Fix naming rule violation

--- GameServer/PlayerConnectionData.cs
+++ GameServer/PlayerConnectionData.cs
@@ -7,10 +7,10 @@ namespace GameServer;
 /// </summary>
 /// <param name="Nickname">The nickname of the player.</param>
 /// <param name="TypeOfPacketType">The type of packet type to use for the player.</param>
-/// <param name="quickJoin">A value indicating whether the player joined the game by quick join.</param>
+/// <param name="QuickJoin">A value indicating whether the player joined the game by quick join.</param>
 public record struct PlayerConnectionData(
 #if DEBUG
-    string Nickname, TypeOfPacketType TypeOfPacketType, bool quickJoin);
+    string Nickname, TypeOfPacketType TypeOfPacketType, bool QuickJoin);
 #else
     string Nickname, TypeOfPacketType TypeOfPacketType);
 #endif
