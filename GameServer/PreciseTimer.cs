10563433b212d13f55e588de95092b88e6043bc5
Wiktor Jaworski <wijaweq@gmail.com>
2024-10-09 10:05:09 +0200

    Adapt PreciseTimer to GameServer build configuration

--- GameServer/PreciseTimer.cs
+++ GameServer/PreciseTimer.cs
@@ -1,8 +1,12 @@
 ﻿using System.Diagnostics;
+
+#if WINDOWS
 using System.Runtime.InteropServices;
+#endif
 
 namespace GameServer;
 
+#pragma warning disable IDE0079
 #pragma warning disable SYSLIB1054
 
 /// <summary>
@@ -14,13 +18,12 @@ internal class PreciseTimer
     /// Delays the current thread for the specified number of milliseconds.
     /// </summary>
     /// <param name="milliseconds">The number of milliseconds to delay the current thread.</param>
-    /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
+    /// <returns>A task representing the asynchronous operation.</returns>
     public static async Task PreciseDelay(int milliseconds)
     {
-        if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
-        {
-            _ = TimeBeginPeriod(1);
-        }
+#if WINDOWS
+        _ = TimeBeginPeriod(1);
+#endif
 
         var sw = Stopwatch.StartNew();
         var sleepMs = milliseconds - 1;
@@ -35,15 +38,18 @@ internal class PreciseTimer
             Thread.SpinWait(1);
         }
 
-        if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
-        {
-            _ = TimeEndPeriod(1);
-        }
+#if WINDOWS
+        _ = TimeEndPeriod(1);
+#endif
     }
+
+#if WINDOWS
 
     [DllImport("winmm.dll", EntryPoint = "timeBeginPeriod", SetLastError = true)]
     private static extern uint TimeBeginPeriod(uint uMilliseconds);
 
     [DllImport("winmm.dll", EntryPoint = "timeEndPeriod", SetLastError = true)]
     private static extern uint TimeEndPeriod(uint uMilliseconds);
+
+#endif
 }
